06df599057da1ffb2a6b8fb7d9b5227319cb49be
yhtsao <konve123.am99@gmail.com>
2018-11-30 13:19:37 +0800

    Add query hotels by city and sort in ASC or DESC order

--- app/controllers/HotelQueryController.scala
+++ app/controllers/HotelQueryController.scala
@@ -2,18 +2,31 @@ package controllers
 
 import javax.inject.{Inject, Singleton}
 
-import io.ReadCSV
 import models.Hotel
 import play.api.libs.json._
 import play.api.mvc._
-
+import services.ReadCSV
 
 @Singleton
 class HotelQueryController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def queryHotel = Action {
+  def queryHotel(sort : Option[String], city : Option[String]) = Action {
+
+    // Initialize Json format for Hotel
     implicit val hotelFormat = Json.writes[Hotel]
-    val hotelList = ReadCSV.getHotels
+
+    // Get hotel list
+    var hotelList = ReadCSV.getHotels
+
+    if (!sort.isEmpty) {
+      if (sort.get.equals("asc"))
+        hotelList = hotelList.sortWith(_.price < _.price)
+      else
+        hotelList = hotelList.sortWith(_.price > _.price)
+    }
+
+    if (!city.isEmpty)
+      hotelList = hotelList.filter(_.city.toLowerCase.startsWith(city.get.toLowerCase()))
 
     Ok(views.html.hotel(hotelList))
   }
